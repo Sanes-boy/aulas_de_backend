0c1b729e783054ee83d5aa1533ec1bb4c7230544
Paulo Guilherme Bezerra da Silva <16723219643@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-02-27 08:42:46 -0300

    aula de vetores

---
+++ aula 6/vetor.php
@@ -0,0 +1,53 @@
+<h2>Vetores (array)</h2>
+<p>
+    São estruturas de armazenamento compostas heterogêneas, 
+    que podem ser indexadas númericamente ou textualmente.
+</p>
+
+<?php
+    //exemplo variável simples
+    $nota = 6;
+    $nota = 3;
+    echo $nota . "<br>";
+
+    //exemplo vetor indexado númericamente
+    $notas = []; //outra forma $nota = array();
+    $notas[] = 6;
+    $notas[] = 3;
+    //var_dump ($notas);
+    //imprimir só a primeira nota
+    echo ($notas[0] + $notas[1]) / 2 ;
+
+    //Vetores com índice textual
+    $notas = []; //cria um vetor vazio
+    $notas["debora"] = 7;
+    $notas["murilo"] = 7.8;
+    $notas["poliana"] = 9;
+    echo "<p></p>";
+    echo "A Poliana tirou nota" . $notas['poliana'];
+
+    //imprimindo o conteúdo de um vetor numérico
+  
+    $valores = [];
+    $valores[0] = 2;
+    $valores[1] = 4;
+    $valores[2] = 8;
+    $valores[3] = 10;
+    $valores[4] = 12;
+    $valores[5] = 14;
+    $valores[6] = 16;
+
+    //ou - $valores = [2, 4, 8, 10, 12, 14, 16];
+    //imprimindo o conteúdo de um vetor numérico
+    echo "<br>";
+    for($i = 0; $i <= 6; $i++){
+          echo $valores[$i] . "<br>";
+    }
+   
+    //somar os valores de um vetor
+    $acumulador = 0;
+    for($i=0; $i<=6; $i++){
+        $acumulador = $acumulador + $valores[$i];
+    }
+    echo "<br> A soma dos valores é $acumulador";
+?>
