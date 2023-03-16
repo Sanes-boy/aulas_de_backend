9b7eb9812be50f1e7a779444a9a24cceaa1a5b43
Paulo Guilherme Bezerra da Silva <16723219643@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-16 11:34:03 -0300

    aula 11

---
+++ aula 11/alunos.php
@@ -0,0 +1,29 @@
+<?php
+    $aluno1 = [
+        "nome" => "Poliana Silveira",
+        "nota1" => 7,
+        "nota2" => 9
+        ];
+
+    $aluno2 = [
+        "nome" => "Murilo Alexandro",
+        "nota1" => 8,
+        "nota2" => 6
+        ];
+
+    $aluno3 = array (
+        "nome" => "André Felipe",
+        "nota1" => 6,
+        "nota2" => 7
+    );
+
+    $aluno4 = array (
+        "nome" => "Lavínia Sandy",
+        "nota1" => 6,
+        "nota2" => 9
+    );
+    $dados = [$aluno1, $aluno2, $aluno3, $aluno4];
+
+    //print_r($dados);
+    echo json_encode ($dados);
+    ?>
