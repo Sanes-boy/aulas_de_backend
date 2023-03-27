725f5e603d6f845140475abadc09de32d7409d92
Paulo Guilherme Bezerra da Silva <16723219643@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-27 08:52:50 -0300

    aula mudando de cor

--- aula 15/atividade1.php
+++ aula 15/atividade1.php
@@ -1,5 +1,10 @@
 <?php
     $cores = ["yellow", "green", "blue", "pink", "red", "purple", "orange", "gray", "white"];
+    if(isset($_GET['cor'])){
+        $cor = $_GET['cor'];
+    }else{
+        $cor = 'white';
+    }
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +14,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body{
+        background-color: <?php echo $cor ?>;
+        }
+    </style>
 </head>
 <body>
     
